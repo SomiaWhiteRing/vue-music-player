d99192a8c8ea076b388b8e5ebdd252b3f796b828
SomiaWhiteRing <55022033+SomiaWhiteRing@users.noreply.github.com>
2021-08-16 09:03:38 +0800

    通过对vue常用指令的应用与ref实现播放音乐

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const vm = new Vue({
     el: '#app',
     data: {
+      //音乐标题
       title: '音乐标题',
+      //歌手名称
       author: '歌手名称',
-    }
+      //音乐来源
+      src:"https://static.xhxly.cn/Reality.mp3",
+      //播放状态检测
+      isPlaying:false,
+    },
+    methods: {
+      playMusic(){
+        this.$refs.player.play();
+        this.isPlaying = true;
+      },
+      pauseMusic(){
+        this.$refs.player.pause();
+        this.isPlaying = false;
+      },
+    },
   })
